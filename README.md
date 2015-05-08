c6eaa3f80b112ff79f5cf866657aacf8e5fb0f13
AJ <aj.nice@daumkakao.com>
2015-05-08 21:55:46 +0900

    first

---
+++ README.md
@@ -0,0 +1,24 @@
+# 카드이미지 빌더 
+
+[vox의 meme](https://github.com/voxmedia/meme) 카피 
+
+![screenshot](readme.png)
+
+## 개발하기 
+
+### install 
+
+* `git clone git@github.com:niceaji/image-builder.git`
+* `bundle install`
+* `bundle exec middleman`
+
+로컬 웹서버 시작 `http://localhost:4567/`
+
+### static file build 
+
+* `bundle exec middleman build`
+
+## 데모구경 
+
+* http://niceaji.github.com/image-builder
+
---
+++ config.rb
@@ -0,0 +1,47 @@
+# Reload the browser automatically whenever files change
+activate :livereload
+
+###
+# Compass
+###
+compass_config do |config|
+  config.output_style = :compressed
+end
+
+###
+# Helpers
+###
+helpers do
+  def get_url
+    absolute_prefix + url_prefix
+  end
+end
+
+###
+# Config
+###
+set :css_dir, 'stylesheets'
+set :js_dir, 'javascripts'
+set :images_dir, 'images'
+set :url_prefix, '/'
+set :absolute_prefix, 'http://localhost:4567'
+
+# Build-specific configuration
+configure :build do
+  puts "local build"
+  set :http_prefix, "/image-builder"
+  set :url_prefix, ""
+  set :absolute_prefix, ""
+  activate :asset_hash
+  activate :minify_javascript
+  activate :minify_css
+end
+
+activate :deploy do |deploy|
+  deploy.method = :git
+  # Optional Settings
+  # deploy.remote   = 'custom-remote' # remote name or git url, default: origin
+  # deploy.branch   = 'custom-branch' # default: gh-pages
+  # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
+  # deploy.commit_message = 'custom-message'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
+end
